d96f4b64422d70d5fdb4163738ffe9bb8d565b45
Cas van Ooststroom <110369@st-maartenscollege.nl>
2020-06-19 10:22:20 +0000

    tekst toegevoegd

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-name = input('Wat is je naam?\n')
-print('Hallo, %s.' % name)
+print ("Hallo, welkom bij galgje")
 
+name = input('Wat is je naam?\n')
+print('Hoi, %s.' % name)
+print("Het is de bedoeling dat je alle leters goed raad om een woord te vormen.")
+print ("Je hebt wel maar een paar kansen om de letters te raden.")
