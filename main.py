89cb1f8dca6e8876cf8feacb90d6ebe78e47f913
Pepijn Vaessen <110565@st-maartenscollege.nl>
2020-06-19 10:26:06 +0000

    Woorden toegevoegd

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ name = input('Wat is je naam?\n')
 print('Hoi, %s.' % name)
 print("Het is de bedoeling dat je alle leters goed raad om een woord te vormen.")
 print ("Je hebt wel maar een paar kansen om de letters te raden.")
+
+woorden = ["mango", "vuurtoren", "dorp", "bosbes", "plaats", "haai"]
