c4c3480868733ffc7490a78c9445f6caed908de6
Cas van Ooststroom <110369@st-maartenscollege.nl>
2020-06-26 12:03:03 +0000

    turns added

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ print ("Hallo, welkom bij galgje")
 name = input('Wat is je naam?\n')
 print('Hoi, %s.' % name)
 print("Het is de bedoeling dat je alle leters goed raad om een woord te vormen.")
-print ("Je hebt wel maar een paar kansen om de letters te raden.")
+print ("Je hebt maar een paar kansen om de letters te raden.")
 
 import random 
 words = ["mango", "vuurtoren", "muziek", "bosbes", "plaats", "telefoon", "statief"]
 picked = random.choice(words)
 print (picked)
+
+guesses = ''
+
+turns = 10
+
+
+
